4de5babdd71521e6caa2c0d9dcd49cb09bfade90
Reginais <reginasimanjuntak07@gmail.com>
2026-04-02 23:46:39 +0700

    tugas lima

--- tugas_lima/latihan_nilai.php
+++ tugas_lima/latihan_nilai.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang=en>
-    
+<html lang="en">
     <head>
         <title>Latihan Nilai</title>
         <meta charset="UTF-8">
@@ -11,44 +10,44 @@
         <h1>Nilai Mahasiswa</h1>
         
         <form method="post" action="">
-            Nama: <input type="text" name="nama"><br><br>
-            Nilai: <input type="number" name="nilai"><br><br>
-            <input type="submit" value="Proses">
+        Nama: <input type="text" name="nama"><br><br>
+        Nilai: <input type="text" name="nilai"><br><br>
+        <input type="submit" value="Proses">
         </form>
 
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nama = $_POST['nama'];
-        $nilai = $_POST['nilai'];
+        <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $nama = $_POST['nama'];
+                $nilai = $_POST['nilai'];
 
-        // Menentukan predikat dari nilai
-        if ($nilai >= 85 && $nilai <= 100) {
-            $predikat = "A";
-        } elseif ($nilai >= 75) {
-            $predikat = "B";
-        } elseif ($nilai >= 65) {
-            $predikat = "C";
-        } elseif ($nilai >= 50) {
-            $predikat = "D";
-        } elseif ($nilai >= 0) {
-            $predikat = "E";
-        } else {
-            $predikat = "Tidak valid";
-        }
+                // Menentukan predikat dari nilai
+                if ($nilai >= 85 && $nilai <= 100) {
+                    $predikat = "A";
+                } elseif ($nilai >= 75) {
+                    $predikat = "B";
+                } elseif ($nilai >= 65) {
+                    $predikat = "C";
+                } elseif ($nilai >= 50) {
+                    $predikat = "D";
+                } elseif ($nilai >= 0) {
+                    $predikat = "E";
+                } else {
+                    $predikat = "Tidak Valid";
+                }
 
-        // Status lulus atau tidak
-        if ($nilai >= 60) {
-            $status = "Lulus";
-        } else {
-            $status = "Tidak Lulus";
-        }
+                // Ketentuan status lulus atau tidak
+                if ($nilai >= 60) {
+                    $status = "Lulus";
+                } else {
+                    $status = "Tidak Lulus";
+                }
 
-        echo "<h3>Hasil:</h3>";
-        echo "Nama: $nama <br>";
-        echo "Nilai: $nilai <br>";
-        echo "Predikat: $predikat <br>";
-        echo "Status: $status";
-        }
-    ?>
+            echo "<h3>Hasil:</h3>";
+            echo "Nama: $nama <br>";
+            echo "Nilai: $nilai <br>";
+            echo "Predikat: $predikat <br>";
+            echo "Status: $status";
+            }
+        ?>
     </body>
 </html>
